c045d1e343eb2782c4dd1a02ddeaa9a6eb7944b5
denos <costelasdenis@gmail.com>
2023-06-03 21:26:51 +0300

    added individual item for each listing model

---
+++ lib/models/item.dart
@@ -0,0 +1,10 @@
+class Item {
+  final String title;
+  final String photoUrl;
+  final double pricePaid;
+  final double priceMarket;
+  final int amountOfItem;
+
+  Item(this.title, this.photoUrl, this.pricePaid, this.priceMarket,
+      this.amountOfItem);
+}
