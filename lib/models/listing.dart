9bc500a36843c48709e0c158fad878a95ea81fe0
denos <costelasdenis@gmail.com>
2023-06-03 21:38:03 +0300

    added items to each listing template

--- lib/models/listing.dart
+++ lib/models/listing.dart
@@ -1,12 +1,17 @@
+import 'package:gestionare_prod/models/item.dart';
+
 class Listing {
   final String id;
   final String title;
   final DateTime dateTime;
   final double amount;
+  final List<Item> itemList;
 
-  Listing(
-      {required this.id,
-      required this.title,
-      required this.dateTime,
-      required this.amount});
+  Listing({
+    required this.id,
+    required this.title,
+    required this.dateTime,
+    required this.amount,
+    required this.itemList,
+  });
 }
