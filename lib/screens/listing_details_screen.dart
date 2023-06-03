07b09b25e2d21c495d6a97a14db23abb5ea0750a
denos <costelasdenis@gmail.com>
2023-06-03 21:45:19 +0300

    added listing details screen logic

---
+++ lib/screens/listing_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListingDetailsScreen extends StatelessWidget {
+  static const routeName = "/listing-details-screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
