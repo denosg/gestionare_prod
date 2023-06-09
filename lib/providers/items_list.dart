b0190309cea9e14142aef83988389b77ddfcff07
denos <costelasdenis@gmail.com>
2023-06-09 22:11:56 +0300

    added items list notifier for listview

---
+++ lib/providers/items_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/item.dart';
+
+class ItemListProvider with ChangeNotifier {
+  // list of the items in the listing
+  List<Item> _itemsList = [];
+
+  List<Item> get itemsList {
+    return [..._itemsList];
+  }
+
+  void addItemInList(Item currentItem) {
+    _itemsList.insert(0, currentItem);
+    notifyListeners();
+  }
+}
